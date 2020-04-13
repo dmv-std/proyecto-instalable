fdb36f68c48ac922252742f6cd450e856958bda4
lolipalooza <mumifag_lalolanda@hotmail.com>
2020-04-13 12:20:45 -0400

    fix

---
+++ public_html/assets/pdf/generar-pdf.php
@@ -0,0 +1,19 @@
+<?php require_once "$basepath/librerias/vendor/autoload.php";
+
+use Spipu\Html2Pdf\Html2Pdf;
+use Spipu\Html2Pdf\Exception\Html2PdfException;
+use Spipu\Html2Pdf\Exception\ExceptionFormatter;
+
+$html2pdf = new Html2Pdf('P','A4','es',true,'UTF-8');
+
+$html2pdf->writeHTML($html);
+if ( isset($_GET['mode']) ) {
+	
+	if ( $_GET['mode'] == "saveas" ) {
+		$html2pdf->output("$filename.pdf", 'D');
+	} else if ( $_GET['mode'] == "storein" ) {
+		$html2pdf->output("$pdf_path/$filename"."_$id.pdf", 'F');
+	} else $html2pdf->output("$filename.pdf");
+	
+}
+else $html2pdf->output("$filename.pdf");
